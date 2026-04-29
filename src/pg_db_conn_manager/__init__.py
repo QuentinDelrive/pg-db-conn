ef6441e88303a45388ff6e48e908e28238b67dfc
QuentinDelrive <q.delrive@cma-hautsdefrance.fr>
2026-04-29 14:56:19 +0200

    ajout des types dans l'api public

--- src/pg_db_conn_manager/__init__.py
+++ src/pg_db_conn_manager/__init__.py
@@ -1,4 +1,4 @@
 from .models import DBConnection
-from ._types import CopyDir
+from ._types import CopyDir, FormatCopyVal, OptionsCopy
 
-__all__ = ["DBConnection", "CopyDir"]
+__all__ = ["DBConnection", "CopyDir", "OptionsCopy", "FormatCopyVal"]
